439ea4f80e23f4b7ad408af772977400b053a5de
Renan Tafner <renantafner@gmail.com>
2020-09-02 21:18:15 -0300

    Includes the Hello World JS code

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function helloWorld() { 
+    for (let i = 3; i > 0; i-- ) {
+         console.log(i); 
+    } 
+    
+    console.log('...'); 
+    console.log('Hello World!'); 
+} 
+
+helloWorld();
