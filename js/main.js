c7c6636e28f8f9b3583cb680ce6630444e952614
Renan Tafner <renantafner@gmail.com>
2020-09-04 13:36:32 -0300

    ajustes v1.0.5

--- js/main.js
+++ js/main.js
@@ -18,7 +18,8 @@ function successCallback (position) {
 
         var coordenadas = latitude +',' + longitude;
 
-        var img_url = "https://maps.googleapis.com/maps/api/staticmap?center="+coordenadas+"&amp;zoom=14&amp;size=400x300&amp;key=AIzaSyAa8HeLH2lQMbPeOiMlM9D1VxZ7pbGQq8o";
+       // var img_url = "https://maps.googleapis.com/maps/api/staticmap?center="+coordenadas+"&amp;zoom=14&amp;size=400x300&amp;key=AIzaSyAa8HeLH2lQMbPeOiMlM9D1VxZ7pbGQq8o";
+       var img_url = "https://maps.googleapis.com/maps/api/staticmap?center="+coordenadas+"&zoom=13&size=600x300&markers=color:red%7Clabel:L%7C"+coordenadas+"&key=AIzaSyAa8HeLH2lQMbPeOiMlM9D1VxZ7pbGQq8o";
 
        document.getElementById('endereco').innerHTML = "<img src='"+img_url+"' width='1000' height='600'>";
 
