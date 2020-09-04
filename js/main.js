1331416aa0bb8b7c9da6e525bf9f81db6567ec44
Renan Tafner <renantafner@gmail.com>
2020-09-03 21:50:15 -0300

    Exerício Prático 01

--- js/main.js
+++ js/main.js
@@ -1,10 +1,32 @@
-function helloWorld() { 
-    for (let i = 3; i > 0; i-- ) {
-         console.log(i); 
-    } 
-    
-    console.log('...'); 
-    console.log('Hello World!'); 
+function determinarLocalizacao() { 
+    if ('geolocation' in navigator) {
+
+        navigator.geolocation.getCurrentPosition(successCallback,errorCallback);
+
+    }else{
+        document.getElementById('endereco').innerHTML = 'Erro: Os recursos de geolocalização não são suportados pelo navegador';
+    }
+
+
+
 } 
 
-helloWorld();
+function successCallback (position) {
+
+        var latitude = position.coords.latitude;
+        var longitude = position.coords.longitude;
+
+        var coordenadas = latitude +',' + longitude;
+
+        var img_url = "https://maps.googleapis.com/maps/api/staticmap?center="+coordenadas+"&amp;zoom=14&amp;size=400x300&amp;key=AIzaSyAa8HeLH2lQMbPeOiMlM9D1VxZ7pbGQq8o";
+
+       document.getElementById('endereco').innerHTML = "<img src='"+img_url+"' width='1000' height='600'>";
+
+}
+
+function errorCallback (error) {
+    document.getElementById('endereco').innerHTML = 'Erro: Os recursos de geolocalização não foram permitidos pelo navegador';
+}
+
+
+determinarLocalizacao();
