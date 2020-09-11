bd916e8115ae757a6b5eba89ce5e5e83b2703852
Renan Tafner <renantafner@gmail.com>
2020-09-10 21:14:26 -0300

    Exercise 02

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
 function determinarLocalizacao() { 
+
     if ('geolocation' in navigator) {
+
+       var nome = document.getElementById('nome').value;
+
+        if(nome===""){
+            alert("Favor digitar o seu nome.")
+            return;
+        }
+
 
         navigator.geolocation.getCurrentPosition(successCallback,errorCallback);
 
     }else{
         document.getElementById('endereco').innerHTML = 'Erro: Os recursos de geolocalização não são suportados pelo navegador';
     }
-
-
 
 } 
 
@@ -17,17 +24,15 @@ function successCallback (position) {
         var longitude = position.coords.longitude;
 
         var coordenadas = latitude +',' + longitude;
-
-       // var img_url = "https://maps.googleapis.com/maps/api/staticmap?center="+coordenadas+"&amp;zoom=14&amp;size=400x300&amp;key=AIzaSyAa8HeLH2lQMbPeOiMlM9D1VxZ7pbGQq8o";
+    
        var img_url = "https://maps.googleapis.com/maps/api/staticmap?center="+coordenadas+"&zoom=13&size=600x300&markers=color:red%7Clabel:L%7C"+coordenadas+"&key=AIzaSyAa8HeLH2lQMbPeOiMlM9D1VxZ7pbGQq8o";
 
-       document.getElementById('endereco').innerHTML = "<img src='"+img_url+"' width='1000' height='600'>";
+       var nome = document.getElementById('nome').value;
+
+       document.getElementById('endereco').innerHTML = "<p>Bem vindo " + nome +" , Resultado da consulta:</p><img class = 'body__main__section__image--result' src='"+img_url+"' width='1000' height='600'>";
 
 }
 
 function errorCallback (error) {
     document.getElementById('endereco').innerHTML = 'Erro: Os recursos de geolocalização não foram permitidos pelo navegador';
 }
-
-
-determinarLocalizacao();
