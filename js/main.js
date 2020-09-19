3ccae71fc227ca1ce1e36ea69ec49b3285df5de3
Renan Tafner <renantafner@gmail.com>
2020-09-18 21:13:03 -0300

    Refactors the to use template literals

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 function determinarLocalizacao() { 
 
-    if ('geolocation' in navigator) {
+    if (`geolocation` in navigator) {
 
-       let nome = document.getElementById('nome').value;
+       let nome = document.getElementById(`nome`).value;
 
-        if(nome===""){
-            alert("Favor digitar o seu nome.")
+        if(nome===``){
+            alert(`Favor digitar o seu nome.`)
             return;
         }
 
@@ -14,7 +14,7 @@ function determinarLocalizacao() {
         navigator.geolocation.getCurrentPosition(...functions);
 
     }else{
-        document.getElementById('endereco').innerHTML = 'Erro: Os recursos de geolocalização não são suportados pelo navegador';
+        document.getElementById(`endereco`).innerHTML = `Erro: Os recursos de geolocalização não são suportados pelo navegador`;
     }
 
 } 
@@ -26,14 +26,14 @@ function successCallback (position) {
 
         const coordenadas = latitude +',' + longitude;
     
-       let img_url = "https://maps.googleapis.com/maps/api/staticmap?center="+coordenadas+"&zoom=13&size=600x300&markers=color:red%7Clabel:L%7C"+coordenadas+"&key=AIzaSyAa8HeLH2lQMbPeOiMlM9D1VxZ7pbGQq8o";
+       let img_url = `https://maps.googleapis.com/maps/api/staticmap?center=${coordenadas}&zoom=13&size=600x300&markers=color:red%7Clabel:L%7C"${coordenadas}"&key=AIzaSyAa8HeLH2lQMbPeOiMlM9D1VxZ7pbGQq8o`;
 
-       let nome = document.getElementById('nome').value;
+       let nome = document.getElementById(`nome`).value;
 
-       document.getElementById('endereco').innerHTML = "<p>Bem vindo " + nome +" , Resultado da consulta:</p><img class = 'body__main__section__image--result' src='"+img_url+"' width='1000' height='600'>";
+       document.getElementById(`endereco`).innerHTML = `<p>Bem vindo ${nome} , Resultado da consulta:</p><img class = 'body__main__section__image--result' src='${img_url}' width='1000' height='600'>`;
 
 }
 
 function errorCallback (error) {
-    document.getElementById('endereco').innerHTML = 'Erro: Os recursos de geolocalização não foram permitidos pelo navegador';
+    document.getElementById(`endereco`).innerHTML = `Erro: Os recursos de geolocalização não foram permitidos pelo navegador`;
 }
